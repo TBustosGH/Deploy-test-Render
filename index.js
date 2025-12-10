09b533565f516fc5bb3e6097331b5a4ac097de6d
TBustosGH <tnbustos2007@gmail.com>
2025-12-10 18:25:35 -0300

    feat: validations & uibuild -Add validations on POST and PUT method -Now the phone numbers have format (DD-...D or DDD-...D)

--- index.js
+++ index.js
@@ -1,74 +1,81 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/Person')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.static('dist'))
-morgan.token('PostData', (request, response) => JSON.stringify(request.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms /// :PostData'))
 
-let phonebook = [
-    {   "id": 1,  "name": "Arto Hellas",    "number": "040-123456"    },
-    {   "id": 2,  "name": "Ada Lovelace",   "number": "39-44-5323523" },
-    {   "id": 3,  "name": "Dan Abramov",    "number": "12-43-234345"  },
-    {   "id": 4,  "name": "Mary Poppendieck",   "number": "39-23-6423122" }
-]
+//morgan.token('PostData', (request, response) => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
-//o
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') 
+        return response.status(400).send({ error: 'malformatted id' })
+    else
+        return response.status(400).send({ error: `${error.name}, ${error.message}` })
+
+    next(error)
+}
 
 //GET: all the phonebook
 app.get('/api/persons', (request, response) => {
-    response.json(phonebook)
+    Person.find({})
+        .then(persons => {
+            response.json(persons)
+        })
 })
 //GET: just a person from the phonebook by it's ID
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = phonebook.find(person => person.id === id)
-
-    if (person)
-        response.json(person)
-    else
-        response.status(404).end()
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            response.json(person)
+        })
+        .catch(error => next(error))
 })
 
 //GET: information from the API. How much persons it has in the phonebook & the actual date.
 app.get('/info', (request, response) => {
-    const amountOfPersons = phonebook.length
-    const now = new Date()
-    const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'September', 'August', 'October', 'November', 'December']
-
-    response.send(
-        `<div> 
-            <h2>Phonebook has info for ${amountOfPersons} people</h2> 
-            <br /> 
-            <p>Today is 
-                ${week[now.getDay() - 1]}, 
-                ${months[now.getMonth()]}   ${now.getDate()}    ${now.getFullYear()}
-            </p> 
-            <p>Time:
-                ${now.getHours()}/${now.getMinutes()}/${now.getSeconds()}
-            </p>
-        </div>`
-    )
+    Person.find({})
+        .then(result => {
+            const amountOfPersons = result.length
+            const now = new Date()
+            const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+            const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'September', 'August', 'October', 'November', 'December']
+        
+            response.send(
+                `<div> 
+                    <h2>Phonebook has info for ${amountOfPersons} people</h2> 
+                    <br /> 
+                    <p>Today is 
+                        ${week[now.getDay() - 1]}, 
+                        ${months[now.getMonth()]}   ${now.getDate()}    ${now.getFullYear()}
+                    </p> 
+                    <p>Time:
+                        ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}
+                    </p>
+                </div>`
+            )
+        })
 })
 
 
 //DELETE: a person from the phonebook by it's ID
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const newPhonebook = phonebook.filter(person => person.id !==id)
-    phonebook = newPhonebook
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    const id = String(request.params.id)
+    Person.findByIdAndDelete(id)
+        .then(result => response.status(204).end())
+        .catch(error => next(error))
 })
 
 
 //POST: a person to the phonebook. name & number required!
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-    const min = 5, max = 999999999999
 
     //Error management
     if (!body)  //Checks if the request is empty
@@ -83,25 +90,37 @@ app.post('/api/persons', (request, response) => {
         return response.status(400).json({
             error: 'No number in request'
         })
-    else if (phonebook.find(person => person.name === body.name))   //Checks if the request's name is already on the phonebook
-        return response.status(400).json({
-            error: 'Name is already added to the phonebook'
-        })
 
 
-    const person = {
-        "id" : Math.floor(Math.random() * (max - min + 1)) + min,
+    const personObject = new Person({
         "name" : body.name,
         "number" : String(body.number)
-    }
-    //console.log('Person: ', person)
-    phonebook = phonebook.concat(person)
-    //console.log('Phonebook: ', phonebook)
-    response.json(phonebook)
+    })
+
+    personObject.save()
+        .then(result => {
+            response.json(personObject)
+        })
+        .catch(error => next(error))
+})
+
+//PUT: a person from the phonebook. name & number required!
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+
+    Person.findByIdAndUpdate(
+        request.params.id, 
+        { name, number }, 
+        { new: true, runValidators: true, context: 'query' }
+    )
+        .then(updatedPerson => response.json(updatedPerson))
+        .catch(error => next(error))
 })
 
 
-const PORT = 3001
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
---
+++ models/Person.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+//Get the url
+const url = process.env.MONGODB_URI
+
+console.log('Connecting to: ', url)
+//Connect to MongoDB
+mongoose.connect(url)
+    .then(result => {
+        console.log('Connected to MongoDB.')
+    })
+    .catch(error => {
+        console.log('Unable to connect to MongoDB: ', error.message)
+    })
+
+//Create the schema
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: (v) => /\d{3}-\d/.test(v) || /\d{2}-\d/,
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: true
+    }
+})
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
